98d7a3ee304857383a7805893c2465f513aefe26
Rezis Github <cosmonshop@gmail.com>
2026-03-06 18:54:00 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Change 'inventory-app' to your actual GitHub repository name
-// Example: if your repo URL is github.com/username/my-inventory → base: "/my-inventory/"
 export default defineConfig({
   plugins: [react()],
-  base: "/inventory-app/",
 });
